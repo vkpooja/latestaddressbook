0587b51bb7f649e92e8a3ee9bf1be754e85aed5a
b <l>
2022-03-22 18:28:18 +0530

    done

--- ansible/prepare-ACM.sh
+++ ansible/prepare-ACM.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 pip3 install ansible
 pip3 install boto3
-set AWS_ACCESS_KEY_ID $1
-set AWS_SECRET_ACCESS_KEY $2
+set AWS_ACCESS_KEY_ID=$1
+set AWS_SECRET_ACCESS_KEY=$2
 /home/ec2-user/.local/bin/ansible-inventory -i /home/ec2-user/ansible/inventory_aws_ec2.yml --list
