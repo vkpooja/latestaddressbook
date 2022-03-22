ca2387c0b62d0536e287695e9ee14c9af3719866
b <l>
2022-03-22 15:54:59 +0530

    done

--- ansible/prepare-ACM.sh
+++ ansible/prepare-ACM.sh
@@ -2,5 +2,5 @@
 pip3 install ansible
 pip3 install boto3
 mkdir /home/ec2-user/.aws 
-cp .aws-credentials /home/ec2-user/.aws/credentials
+cp ./aws-credentials /home/ec2-user/.aws/credentials
 /home/ec2-user/.local/bin/ansible-inventory -i /home/ec2-user/ansible/inventory_aws_ec2.yml --list
