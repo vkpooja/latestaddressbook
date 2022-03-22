5b473f475bdecbf6fe49ec3e79d0beefa3439981
b <l>
2022-03-22 19:33:16 +0530

    done

--- ansible/prepare-ACM.sh
+++ ansible/prepare-ACM.sh
@@ -4,4 +4,4 @@ pip3 install boto3
 aws configure set aws_access_key_id $1
 aws configure set aws_secret_access_key $2
 /home/ec2-user/.local/bin/ansible-inventory -i /home/ec2-user/ansible/inventory_aws_ec2.yml --graph
-DOCKER_IMAGE=$3 /home/ec2-user/.local/bin/ansible-playbook -i /home/ec2-user/ansible/inventory_aws_ec2.yml ansible_ssh_common_args='-o StrictHostKeyChecking=no' /home/ec2-user/ansible/docker-deploy.yml
+DOCKER_IMAGE=$3 /home/ec2-user/.local/bin/ansible-playbook -i /home/ec2-user/ansible/inventory_aws_ec2.yml  /home/ec2-user/ansible/docker-deploy.yml
