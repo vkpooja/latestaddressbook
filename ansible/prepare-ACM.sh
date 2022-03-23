411e34ed566989634f93992dcef2bec26bca7a50
b <l>
2022-03-23 09:07:12 +0530

    TF Changes

--- ansible/prepare-ACM.sh
+++ ansible/prepare-ACM.sh
@@ -5,3 +5,4 @@ aws configure set aws_access_key_id $1
 aws configure set aws_secret_access_key $2
 /home/ec2-user/.local/bin/ansible-inventory -i /home/ec2-user/ansible/inventory_aws_ec2.yml --graph
 DOCKER_IMAGE=$3 /home/ec2-user/.local/bin/ansible-playbook -i /home/ec2-user/ansible/inventory_aws_ec2.yml  /home/ec2-user/ansible/docker-deploy.yml
+rm /home/ec2-user/.ssh/id_rsa -y
