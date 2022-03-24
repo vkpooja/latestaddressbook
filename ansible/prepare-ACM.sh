cf03cf49c83115cbd3527bcd680a510f097deea7
b <l>
2022-03-24 09:23:07 +0530

    complete code

--- ansible/prepare-ACM.sh
+++ ansible/prepare-ACM.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/env bash
-pip3 install ansible
-pip3 install boto3
+sudo amazon-linux-extras install ansible2
+sudo yum install python-boto3 -y
 aws configure set aws_access_key_id $1
 aws configure set aws_secret_access_key $2
-/home/ec2-user/.local/bin/ansible-inventory -i /home/ec2-user/ansible/inventory_aws_ec2.yml --graph
-DOCKER_IMAGE=$3 /home/ec2-user/.local/bin/ansible-playbook -i /home/ec2-user/ansible/inventory_aws_ec2.yml  /home/ec2-user/ansible/docker-deploy.yml
+ansible-inventory -i /home/ec2-user/inventory_aws_ec2.yml --graph
+chmod 400 /home/ec2-user/.ssh/id_rsa
+ansible-playbook -i /home/ec2-user/inventory_aws_ec2.yml  /home/ec2-user/docker-deploy.yml -e "DOCKER_IMAGE=$4 DOCKER_REG_PASSWORD=$3"
+rm /home/ec2-user/.ssh/id_rsa
